a3392089c61a2e4bef14478ae1746b9cdca23836
Jimmy Zhang <jimmyzhang2003@gmail.com>
2024-03-18 17:46:40 -0400

    Minor cosmetic changes

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,60 +1,60 @@
 import React, { useState } from "react";
 
 function CreateNote(props) {
-  const [note, setNote] = useState({
-    title: "",
-    content: "",
-  });
+	const [note, setNote] = useState({
+		title: "",
+		content: "",
+	});
 
-  // update note state upon editing title or content
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setNote((prevNote) => {
-      return {
-        ...prevNote,
-        [name]: value,
-      };
-    });
-  };
+	// update note state upon editing title or content
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setNote((prevNote) => {
+			return {
+				...prevNote,
+				[name]: value,
+			};
+		});
+	};
 
-  // upon clicking add button, add current note to notes array and update note state
-  const handleSubmit = (e) => {
-    e.preventDefault();
+	// upon clicking add button, add current note to notes array and update note state
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    // add current note to notes array
-    props.onCreate(note);
+		// add current note to notes array
+		props.onCreate(note);
 
-    // after rendering current note, clear input and textfield
-    setNote({
-      title: "",
-      content: "",
-    });
-  };
+		// after rendering current note, clear input and textfield
+		setNote({
+			title: "",
+			content: "",
+		});
+	};
 
-  return (
-    <div className="create-note-container">
-      <form onSubmit={handleSubmit}>
-        {/* use input field for note title */}
-        <input
-          name="title"
-          placeholder="Title"
-          value={note.title}
-          onChange={handleChange}
-        />
-        {/* use textarea for note content */}
-        <textarea
-          name="content"
-          placeholder="Take a note..."
-          rows="4"
-          value={note.content}
-          onChange={handleChange}
-        />
-        <button type="submit" className="add-button">
-          Add
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="create-note-container">
+			<form onSubmit={handleSubmit}>
+				{/* use input field for note title */}
+				<input
+					name="title"
+					placeholder="Title"
+					value={note.title}
+					onChange={handleChange}
+				/>
+				{/* use textarea for note content */}
+				<textarea
+					name="content"
+					placeholder="Take a note..."
+					rows="3"
+					value={note.content}
+					onChange={handleChange}
+				/>
+				<button type="submit" className="add-button">
+					Add
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default CreateNote;
